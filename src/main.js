ba0e8339cb4b8bd6985f81592440cd0806e9b777
Mehdi Barasov <asker.20tr@gmail.com>
2024-03-22 11:40:55 +0400

    vue 3 course

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+/***
+
+ Yeni qayda ile App.vue adinda bir fayl yaratdiq.
+
+ template - icinde html kodlarimiz yaziriq
+ script - icinde js kodlarimiz yaziriq
+ style - icinde css kodlarimiz yaziriq
+
+
+ ***/
+
+createApp(App).mount('#app')
+
+
+
+
